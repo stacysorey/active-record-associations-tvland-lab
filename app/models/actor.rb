a6023d3d9fb312f8592fe8760877ea4d13c0ba52
stacysorey <stacysorey@gmail.com>
2021-01-08 19:54:54 -0500

    commit tree 44d40137c70607a9275749c4ac55092d670e308c

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,16 @@
 class Actor < ActiveRecord::Base
-  
+  #has many characters and has many shows through characters
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    #actor can list its full name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    #actor can list all of its shows and characters
+    characters.collect {|character| "#{character.name} - #{character.show.name}"}
+  end
+
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
+  # a show has many characters and has many actors through characters
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+    # Show has an #actors_list method that returns a list of the full names of each actor associated with the show
+    self.actors.collect {|actor| actor.full_name}
+  end
   
 end
